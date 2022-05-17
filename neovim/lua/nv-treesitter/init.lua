80deafba1e6c6ebaa6894439ffc251db9c02d566
Alexandru Obada <alexandru@drivekyte.com>
2022-05-17 11:17:16 +0200

    Added treesitter for hcl

--- neovim/lua/nv-treesitter/init.lua
+++ neovim/lua/nv-treesitter/init.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = {"lua", "rust", "python", "go"},
+    ensure_installed = {"lua", "rust", "python", "go", "hcl"},
 
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
